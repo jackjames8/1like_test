901ace2259b31c80fe8b08352d9587e153fa73a1
jackjames <369631393@qq.com>
2018-05-29 20:28:15 +0800

    后面改的

--- 1like_test/app/src/main/java/com/example/a1like_test/MainActivity.java
+++ 1like_test/app/src/main/java/com/example/a1like_test/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.a1like_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+1////
 public class MainActivity extends AppCompatActivity {
 
     @Override
